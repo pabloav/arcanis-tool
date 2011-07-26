a8442f540b28bc820ad2f5b165cec91c3913d1b9
Pablo Averbuj <pablo@vanitude.com>
2011-07-26 14:55:50 -0400

    Added monster management

--- app/controllers/dm_controller.rb
+++ app/controllers/dm_controller.rb
@@ -4,7 +4,6 @@ class DmController < ApplicationController
   def index
   end
   
-  # TODO: This needs to move to the DM controller but here for now for debugging
   def tick
     @master_clock.tick!
     refresh
@@ -18,15 +17,34 @@ class DmController < ApplicationController
     refresh
   end
   
-  def refresh
-    load_players
-    render :partial => 'dm_table'
-  end
-
   def reset_scene
     @players.each { |p| p.reset! }
     @master_clock.reset!
     refresh
+  end
+  
+  def perform
+    if params[:player_id].present?
+      @player = Player.find(params[:player_id])
+      @player.perform!(params[:clock].to_i, params[:speed].to_i, params[:strain].to_i, params[:recovery].to_i)
+    end
+    refresh
+  end
+  
+  def add_monster
+    if params[:monster_name].present?
+      p = Player.new
+      p.name = params[:monster_name]
+      p.npc = true
+      p.clock = params[:initiative].to_i || @master_clock.clock
+      p.save
+    end
+    refresh
+  end
+
+  def refresh
+    load_players
+    render :partial => 'dm_table'
   end
 
 private
