b8f2d99a13c3f1252a966bc82f0df719ca664702
Pablo Averbuj <pablo@vanitude.com>
2011-07-26 11:25:41 -0400

    Capistrano config

---
+++ config/deploy.rb
@@ -0,0 +1,24 @@
+set :application, "arcanis-tool"
+set :repository,  "git://github.com/pabloav/arcanis-tool.git"
+
+set :scm, :git
+# Or: `accurev`, `bzr`, `cvs`, `darcs`, `git`, `mercurial`, `perforce`, `subversion` or `none`
+
+role :web, "franklin.vanitude.com"                          # Your HTTP server, Apache/etc
+role :app, "franklin.vanitude.com"                          # This may be the same as your `Web` server
+role :db,  "franklin.vanitude.com", :primary => true # This is where Rails migrations will run
+
+set :deploy_to, "/var/sites/arcanis-tool.vanitude.com/"
+set :rails_env, "production" 
+
+# If you are using Passenger mod_rails uncomment this:
+# if you're still using the script/reapear helper you will need
+# these http://github.com/rails/irs_process_scripts
+
+# namespace :deploy do
+#   task :start do ; end
+#   task :stop do ; end
+#   task :restart, :roles => :app, :except => { :no_release => true } do
+#     run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
+#   end
+# end
