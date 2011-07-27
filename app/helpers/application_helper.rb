a5142f29e92a59409fceb34c66c38f13f4485684
Pablo Averbuj <pablo@vanitude.com>
2011-07-26 20:58:55 -0400

    Updated views: icons, iphone styling for player layout

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,6 @@
 # Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper
+  def timestamp
+    return "<span class=\"timestamp\">(#{Time.now().strftime("%I:%M:%S%P").downcase})</span>"
+  end
 end
